996a97eb24bd7b26304dc8ffb6befbfb938d9238
Prince-3103 <p2577430@gmail.com>
2026-08-14 23:42:28 +0530

    Creating blocks

---
+++ script.js
@@ -0,0 +1,12 @@
+const board = document.querySelector('.board');
+const blockHeight = 30;
+const blockWidth = 30;
+
+const rows = Math.floor(board.clientHeight/blockHeight);
+const cols = Math.floor(board.clientWidth/blockWidth);
+
+for(let i=0; i<rows*cols; i++){
+    const block = document.createElement("div");
+    block.classList.add("block");
+    board.appendChild(block);
+}
