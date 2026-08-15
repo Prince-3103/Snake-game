8318f744f09f00cb345869003b232713fe2b0969
Prince-3103 <p2577430@gmail.com>
2026-08-16 02:01:05 +0530

    Making snake to eat food and update score count

--- script.js
+++ script.js
@@ -9,8 +9,11 @@ const cols = Math.floor(board.clientWidth/blockWidth);
 const gameOver = document.querySelector(".game-over");
 const startBtn = document.querySelector("#start-btn")
 const restartBtn = document.querySelector("#restart-btn");
+const scoreCounter = document.querySelector("#score");
 
 let intervalId = null;
+let food = {x: Math.floor(Math.random()*rows), y: Math.floor(Math.random()*cols)};
+let count = 0;
 
 let snake = [
     {x: 1, y:3},
@@ -29,48 +32,56 @@ for(let row=0; row<rows; row++){
 }
 
 function render(){
+    let head = null;
+    if(direction === "left"){
+        head = {x: snake[0].x, y: snake[0].y - 1};
+    }
+    else if(direction === "right"){
+        head = {x: snake[0].x, y: snake[0].y + 1};
+    }
+    else if(direction === "up"){
+        head = {x: snake[0].x - 1, y: snake[0].y};
+    }
+    else if(direction === "down"){
+        head = {x: snake[0].x + 1, y: snake[0].y};
+    }
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove("filled");
+    })
+    
+    
+    if(head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols){
+        gameOver.style.display = "flex";
+        clearInterval(intervalId);
+        return;
+    }
+    
+    snake.unshift(head);
+    snake.pop()
+
     snake.forEach(segment=>{
         blocks[`${segment.x}-${segment.y}`].classList.add("filled");
     })
+    
+    blocks[`${food.x}-${food.y}`].classList.add("food");
+
+    if(head.x === food.x && head.y === food.y){
+        blocks[`${food.x}-${food.y}`].classList.remove("food");
+        food = {x: Math.floor(Math.random()*rows), y: Math.floor(Math.random()*cols)};
+
+        count++;
+        blocks[`${food.x}-${food.y}`].classList.add("food");
+
+        snake.unshift(head)
+    }
+    scoreCounter.textContent = count;
 }
 
 function startGame(){
-    snake = [
-        {x: 1, y:3},
-        {x: 1, y:4},
-        {x: 1, y:5}
-    ]
+    count = 0;
     intervalId = setInterval(() =>{
-        let head = null;
-        if(direction === "left"){
-            head = {x: snake[0].x, y: snake[0].y - 1};
-        }
-        else if(direction === "right"){
-            head = {x: snake[0].x, y: snake[0].y + 1};
-        }
-        else if(direction === "up"){
-            head = {x: snake[0].x - 1, y: snake[0].y};
-        }
-        else if(direction === "down"){
-            head = {x: snake[0].x + 1, y: snake[0].y};
-        }
-        snake.forEach(segment => {
-            blocks[`${segment.x}-${segment.y}`].classList.remove("filled");
-        })
-    
-    
-        if(head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols){
-            gameOver.style.display = "flex";
-            clearInterval(intervalId);
-            return;
-        }
-        
-        snake.unshift(head);
-        snake.pop()
-    
         render()
-    
-    },400)
+    },300)
 }
 
 startBtn.addEventListener("click", startGame);
@@ -90,5 +101,16 @@ addEventListener("keydown", (event) => {
 });
 
 restartBtn.addEventListener("click", ()=>{
+    snake = [
+        {x: 1, y:3},
+        {x: 1, y:4},
+        {x: 1, y:5}
+    ]
+    direction = "down";
+    blocks[`${food.x}-${food.y}`].classList.remove("food");
+    food = {x: Math.floor(Math.random()*rows), y: Math.floor(Math.random()*cols)};
+
     gameOver.style.display = "none";
+    clearInterval(intervalId);
+
 })
